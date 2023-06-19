6a75bff0eb5fae9179615c6c6af06b62b0e71001
Halim <33067128+ACMUS3@users.noreply.github.com>
2023-06-19 04:59:19 +0300

    Second time commit

--- src/main/java/com/example/springbootapp/runner/MessageRunner.java
+++ src/main/java/com/example/springbootapp/runner/MessageRunner.java
@@ -1,7 +1,9 @@
 package com.example.springbootapp.runner;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MessageRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
